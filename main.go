3212f3e6fcac4f68f8e38108a2c9ac835f986aed
darzhz <darzhz@protonmail.com>
2025-05-18 18:56:08 +0530

    added new functionality

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/pocketbase/pocketbase"
@@ -22,7 +23,8 @@ func main() {
 	})
 	//hook for video upload
 	app.OnRecordCreateRequest("videos").BindFunc(func(e *core.RecordRequestEvent) error {
-		go handleNewUpload(e.Record) // run in background
+		fileName := e.Record.GetUnsavedFiles("video")[0].Name
+		go handleNewUpload(e.Record, fileName) // run in background
 		return e.Next()
 
 	})
@@ -32,13 +34,13 @@ func main() {
 	}
 }
 
-func handleNewUpload(record *core.Record) {
+func handleNewUpload(record *core.Record, fileName string) {
 	// 1. Get uploaded file path
-	filePath := filepath.Join("pb_data", "storage", record.BaseFilesPath(), record.GetString("video"))
+	filePath := filepath.Join("pb_data", "storage", record.BaseFilesPath(), fileName)
 
 	// 2. Encode into 480p and 720p using FFmpeg
-	// encodeVideo(filePath, 480)
-	// encodeVideo(filePath, 720)
+	encodeVideo(filePath, 480)
+	encodeVideo(filePath, 720)
 	fmt.Println("video uploaded")
 	fmt.Println("encoding started at", filePath)
 	// 3. Update the record with encoding status
@@ -47,4 +49,28 @@ func handleNewUpload(record *core.Record) {
 	// if err := db.SaveRecord(record); err != nil {
 	//     log.Println("Failed to update video record:", err)
 	// }
+}
+
+func encodeVideo(filePath string, resolution int) {
+	// Get the video file extension
+	ext := filepath.Ext(filePath)
+	log.Println("extension", ext)
+	log.Println("file path", filePath)
+
+	// Generate the output file path
+	outputPath := fmt.Sprintf("%s_%dp%s", filePath, resolution, ext)
+
+	// Set up the command
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-s", fmt.Sprintf("%dx%d", resolution, resolution), "-c:v", "libx264", "-crf", "18", outputPath)
+
+	// Run the command
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Println(err)
+		log.Println(string(output))
+		return
+	}
+
+	// Log the output
+	log.Printf("Encoded video %s to %s", filePath, outputPath)
 }
