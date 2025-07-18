abd89fc2250f19511c68335ac3bed4342cb83757
darzhz <darzhz@protonmail.com>
2025-07-18 16:56:41 +0530

    changes

--- api/verify.go
+++ api/verify.go
@@ -79,6 +79,90 @@ func SetupVerifyRoutes(app *pocketbase.PocketBase) func(*core.ServeEvent) error 
 				"data":    attendees,
 			})
 		})
+		e.Router.GET("/api/attendees/{eventId}", func(c *core.RequestEvent) error {
+			// list of bookings with isAttended value
+			eventId := c.Request.PathValue("eventId")
+
+			// Get collections
+			bookingCollection, _ := app.FindCollectionByNameOrId("Bookings")
+			attendanceCollection, _ := app.FindCollectionByNameOrId("eventAttendees")
+
+			// Find booking list
+			bookings, err := app.FindRecordsByFilter(
+				bookingCollection,
+				fmt.Sprintf("EventId = '%s'", eventId),
+				"",
+				0, 0,
+			)
+			//expand record
+			errs := app.ExpandRecords(bookings, []string{"UserId", "TicketID"}, nil)
+			if len(errs) > 0 {
+				return c.JSON(http.StatusInternalServerError, map[string]any{
+					"error": "Failed to expand records",
+				})
+			}
+
+			if err != nil {
+				return c.JSON(http.StatusInternalServerError, map[string]any{
+					"message": "Error checking booking records",
+					"status":  "failed",
+				})
+			}
+
+			// Check if already verified
+			attendees, err := app.FindRecordsByFilter(
+				attendanceCollection,
+				fmt.Sprintf("event = '%s'", eventId),
+				"",
+				0, 0,
+			)
+
+			if err != nil {
+				return c.JSON(http.StatusInternalServerError, map[string]any{
+					"message": "Error checking attendee records",
+					"status":  "failed",
+				})
+			}
+			//add a field to booking record called isAttended is bookingid exists in attendees
+			// Build a map of attended booking IDs
+			attendedMap := make(map[string]bool)
+			for _, attendee := range attendees {
+				if bid, ok := attendee.Get("bookingId").(string); ok {
+					attendedMap[bid] = true
+				}
+			}
+
+			// Add isAttended to each booking
+			var finalBookings []map[string]any
+
+			for _, booking := range bookings {
+				id, ok := booking.Get("id").(string)
+				if !ok {
+					return c.JSON(http.StatusInternalServerError, map[string]any{
+						"message": "Invalid booking data (id)",
+						"status":  "failed",
+					})
+				}
+
+				// Copy all booking fields into a new map
+				bookingMap := make(map[string]any)
+				for k, v := range booking.PublicExport() {
+					bookingMap[k] = v
+				}
+
+				// Inject computed field
+				bookingMap["isAttended"] = attendedMap[id]
+
+				finalBookings = append(finalBookings, bookingMap)
+			}
+
+			return c.JSON(http.StatusOK, map[string]any{
+				"message": "Success",
+				"status":  "success",
+				"data":    finalBookings,
+			})
+
+		})
 
 		return e.Next()
 	}
